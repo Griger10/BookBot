5d794710efa0536c57bab7fc01f2bcc836af7804
Grisha Gertzik <griger1011@gmail.com>
2024-07-14 17:00:42 +0300

    other_handler function answer

---
+++ handlers/other_handlers.py
@@ -0,0 +1,8 @@
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+@router.message()
+async def echo(message: Message):
+    await message.answer('Данная команда не поддерживается ботом, воспользуйтесь help')
